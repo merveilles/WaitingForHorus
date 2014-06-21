73b4e190b089748893801e9e1e52a41e35f66f15
cancel <cancel@cancel.fm>
2014-06-21 19:12:10 +0900

    Fix warnings in Spin

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
-using System.Collections;
 
 public class Spin : MonoBehaviour 
 {
 	public float Speed = 1.0f;
 	public Vector3 Axis = Vector3.forward;
-	
-	void Update() 
+
+    public void Update() 
 	{
 		float s = Speed;
 		transform.Rotate( Axis, s * Time.deltaTime );
