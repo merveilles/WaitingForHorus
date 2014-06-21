1d507ab2573e1dfc448b5681c301bb84f17b2e88
cancel <cancel@cancel.fm>
2014-06-21 19:01:03 +0900

    Remove warnings in RemovePerGFXMode

--- Assets/Scripts/RemovePerGFXMode.cs
+++ Assets/Scripts/RemovePerGFXMode.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
-using System.Collections;
 
 public class RemovePerGFXMode : MonoBehaviour 
 {
 	public int MinimumMode = 2;
 	public MonoBehaviour Target;
-	
-	void Awake() 
+
+    public void Awake() 
 	{
-		//print( QualitySettings.GetQualityLevel() );
 		if( QualitySettings.GetQualityLevel() < MinimumMode )
 		{
 			if( Target != null )
