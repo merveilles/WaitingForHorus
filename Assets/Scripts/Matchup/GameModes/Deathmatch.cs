c223a3025cea1df927ea9f8d47c85418a760620d
cancel <cancel@cancel.fm>
2014-06-29 08:00:26 +0900

    Fix dangling characters after restarting server from non-starting map
    We should get a real map loading system soon

--- Assets/Scripts/Matchup/GameModes/Deathmatch.cs
+++ Assets/Scripts/Matchup/GameModes/Deathmatch.cs
@@ -35,12 +35,12 @@ public class Deathmatch : GameMode
         }
 
         CurrentMapName = "pi_mar";
-        Server.ChangeLevel("pi_mar");
-        ReceiveMapChanged();
-        //if (Application.loadedLevelName != "pi_mar")
-        //    Application.LoadLevel("pi_mar");
-        //else
-        //    ReceiveMapChanged();
+
+        // TODO can we get some real map changing here?
+        if (Application.loadedLevelName == "pi_mar")
+            ReceiveMapChanged();
+        else
+            Server.ChangeLevel("pi_mar");
     }
 
 
