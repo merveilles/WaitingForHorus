cb20b4fa6474b801d707bb7e98403e913c991795
cancel <cancel@cancel.fm>
2014-06-28 23:17:19 +0900

    Add some better logging facilities, and make 'em look prettier

--- Assets/Scripts/Utilities/ScreenSpaceDebug.cs
+++ Assets/Scripts/Utilities/ScreenSpaceDebug.cs
@@ -1,10 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 public class ScreenSpaceDebug : MonoBehaviour
 {
     public static ScreenSpaceDebug Instance { get; private set; }
     public bool ShouldDraw = false;
+    public GUISkin Skin;
+
+    private class LineMessage
+    {
+        public string Text;
+        public float Age;
+        public float Lifetime;
+        public Color Color;
+
+        public bool DrawOnce = false;
+        public bool Drawn = false;
+
+        public LineMessage(string text, float lifetime, Color color)
+        {
+            Text = text;
+            Lifetime = lifetime;
+            Color = color;
+            Age = 0f;
+        }
+
+        public void Update()
+        {
+            Age += Time.deltaTime;
+        }
+        public bool Finished {get { return Age >= Lifetime || (DrawOnce && Drawn); }}
+
+        public void DrawLine(GUIStyle style)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Box(Text, style);
+            GUILayout.EndHorizontal();
+            Drawn = true;
+        }
+
+    }
 
     private class Message
     {
@@ -67,18 +104,48 @@ public class ScreenSpaceDebug : MonoBehaviour
     {
         Instance = this;
         Messages = new List<Message>();
+        LineMessages = new Dictionary<int, LineMessage>();
+        AnonymousMessages = new List<LineMessage>();
+        KeysCache = new List<int>();
     }
 
     private List<Message> Messages;
+    private Dictionary<int, LineMessage> LineMessages;
+    private List<LineMessage> AnonymousMessages; 
+
+    private List<int> KeysCache;
+
+    private int LineMessagesHeight
+    {
+        get { return (LineMessages.Count + AnonymousMessages.Count) * 36; }
+    }
 
     public void Update()
     {
+        // Purge old world space messages
         for (int i = Messages.Count - 1; i >= 0; i--)
         {
             Messages[i].Update();
-            if (Messages[i].Finished)
+            if (Messages[i].Finished || (!ShouldDraw && Messages[i].DrawOnce))
                 Messages.RemoveAt(i);
         }
+
+        // Purge old line messages
+        KeysCache.Clear();
+        foreach (KeyValuePair<int, LineMessage> line in LineMessages)
+        {
+            if (line.Value.Finished || (!ShouldDraw && line.Value.DrawOnce)) KeysCache.Add(line.Key);
+        }
+        for (int i = 0; i < KeysCache.Count; i++)
+        {
+            LineMessages.Remove(KeysCache[i]);
+        }
+        for (int i = AnonymousMessages.Count - 1; i >= 0; i--)
+        {
+            if (AnonymousMessages[i].Finished || (!ShouldDraw && AnonymousMessages[i].DrawOnce))
+                AnonymousMessages.RemoveAt(i);
+        }
+
         if (Input.GetKeyDown("f12"))
         {
             ShouldDraw = !ShouldDraw;
@@ -88,10 +155,50 @@ public class ScreenSpaceDebug : MonoBehaviour
     public void OnGUI()
     {
         if (!ShouldDraw) return;
+
+        // Draw world space messages
         for (int i = 0; i < Messages.Count; i++)
         {
             Messages[i].OnGUI();
         }
+
+        // Draw screen space messages
+        KeysCache.Clear();
+        foreach (int i in LineMessages.Keys)
+            KeysCache.Add(i);
+        KeysCache.Sort();
+        GUI.skin = Skin;
+	    GUILayout.Window(3, new Rect( Screen.width - (35 + 300), 35, 300, 800), LineWindow, string.Empty);
+    }
+
+    private void LineWindow(int id)
+    {
+        var basePadding = Skin.box.padding;
+        basePadding.top = 5;
+        basePadding.bottom = 5;
+        var style = new GUIStyle(Skin.box)
+        {
+            fixedWidth = 300,
+            stretchHeight = false,
+            fixedHeight = 0,
+            padding = basePadding
+        };
+        foreach (int i in KeysCache)
+        {
+            LineMessages[i].DrawLine(style);
+        }
+        foreach (var line in AnonymousMessages)
+        {
+            line.DrawLine(style);
+        }
+    }
+
+    public static void LogMessageSizes()
+    {
+        if (Instance == null) return;
+        Debug.LogWarning("World messages: " + Instance.Messages.Count);
+        Debug.LogWarning("Line messages: " + Instance.LineMessages.Count);
+        Debug.LogWarning("Anonymous messages: " + Instance.AnonymousMessages.Count);
     }
 
     public static void AddMessage(string message, Vector3 worldPosition)
@@ -120,4 +227,15 @@ public class ScreenSpaceDebug : MonoBehaviour
         msg.DrawOnce = true;
         Instance.Messages.Add(msg);
     }
+
+    public static void AddLineOnce(string message)
+    {
+        if (Instance == null) return;
+        var line = new LineMessage(message, 1f, Color.white)
+        {
+            DrawOnce = true
+        };
+        Instance.AnonymousMessages.Add(line);
+    }
+
 }
